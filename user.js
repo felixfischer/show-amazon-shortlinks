ca1de92b78fde05cabf89616e3d2d615c42cc78a
Felix Fischer <felix@subtract.de>
2016-09-23 19:06:27 +0200

    Update user.js

--- user.js
+++ user.js
@@ -12,22 +12,35 @@
 (function(doc) {
 
     var ASIN = doc.getElementById("ASIN") || doc.getElementsByName("ASIN.0")[0];
+
     if (ASIN) {
         ASIN = ASIN.value;
         var TLD = window.location.hostname.split('.').pop();
         var URL = `amzn.${TLD}/dp/${ASIN}`;
-        var html = `<div class="a-spacing-large a-text-center a-size-mini" id="my-shortlink">
-                        <a href="http://${URL}" onclick="return false" class="">${URL}</a>
-                    </div>`;
-        var pos = document.getElementById('tell-a-friend');
-        pos.insertAdjacentHTML('afterend', html);
-        //pos.classList.remove('a-spacing-small');
-        //pos.classList.add('a-spacing-none');
+        var html = `
+<div class="a-spacing-large a-text-center" id="my-shortlink">
+<span class="a-button a-button-small a-spacing-mini">
+<span class="a-button-inner">
+Short Link: <a href="http://${URL}" onclick="return false" class="a-button-text" data-orig-text="${URL}">${URL}</a>
+</span>
+</span>
+<p class="navFooterDescText" id="my-shortlink-info">click to copy short link</p>
+</div>
+<style>
+#my-shortlink #my-shortlink-info { opacity: 0; transition: 0.5s; }
+#my-shortlink:hover #my-shortlink-info { opacity: 1; transition: 0.5s; }
+</style>
+`;
+        var pos = document.getElementById('rightCol');
+        pos.insertAdjacentHTML('afterbegin', html);
         var el = document.getElementById('my-shortlink');
         el.onclick = function() {
             GM_setClipboard('http://'+URL);
             var info = document.getElementById('my-shortlink-info');
+            console.log(info);
+            info.innerHTML = 'copied to clipboard';
         };
     }
+
 
 })(document);
